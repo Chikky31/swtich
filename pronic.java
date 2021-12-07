e759dc9e270a89746243b4a6d9de84df7b7f88a7
Chikky31 <82611683+Chikky31@users.noreply.github.com>
2021-12-07 19:19:40 +0530

    Create pronic.java

---
+++ pronic.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class pronic {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n, i, flag = 0;
+        ;
+        System.out.println("enter the number");
+        n = sc.nextInt();
+        for (i = 1; i < (n / 2 - 1); i++) {
+            if (i * (i + 1) == n) {
+                flag = 1;
+              
+                break;
+            } else {
+                continue;
+            }
+        }
+        if (flag == 1) {
+
+            System.out.println("pronic");
+        } else {
+            System.out.println("Not pronic");
+        }
+    }
+}
